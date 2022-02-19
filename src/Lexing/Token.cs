95f633f5eaa727a50b13356350890826978a7460
PaddiM8 <bakk@tuta.io>
2022-02-19 21:17:50 +0100

    Create lexer

---
+++ src/Lexing/Token.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Shel.Lexing;
+
+record struct TextPos(int Line, int Column);
+
+[JsonConverter(typeof(TokenConverter))]
+record Token(TokenKind Kind, string Value, TextPos Position);
+
+class TokenConverter : JsonConverter<Token>
+{
+    public override Token? ReadJson(JsonReader reader, Type objectType, Token? existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void WriteJson(JsonWriter writer, Token? token, JsonSerializer serializer)
+    {
+        writer.WriteValue(token?.Value ?? "");
+    }
+}
