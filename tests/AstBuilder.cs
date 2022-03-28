10e486c2b89ea6dbdbf403d4b57938d0da788efd
PaddiM8 <bakk@tuta.io>
2022-03-28 23:25:15 +0200

    Add interpreter tests

--- tests/AstBuilder.cs
+++ tests/AstBuilder.cs
@@ -8,5 +8,46 @@ static class AstBuilder
     public static Token Token(TokenKind kind, string value)
         => new(kind, value, new TextPos(0, 0));
 
+    public static FunctionExpr Function(string identifier, List<string> parameters, BlockExpr block)
+        => new(
+            Token(TokenKind.Identifier, identifier),
+            parameters.Select(x => Token(TokenKind.Identifier, identifier)).ToList(),
+            block
+        );
 
+    public static LetExpr Let(string identifier, Expr value)
+        => new(Token(TokenKind.Identifier, identifier), value);
+
+    public static ReturnExpr ReturnExpr(Expr value)
+        => new(value);
+
+    public static BinaryExpr Binary(Expr left, TokenKind op, Expr right)
+        => new(left, op, right);
+
+    public static UnaryExpr Unary(TokenKind op, Expr value)
+        => new(op, value);
+
+    public static VariableExpr Var(string identifier)
+        => new(Token(TokenKind.Identifier, identifier));
+
+    public static CallExpr Call(string identifier, List<Expr> arguments)
+        => new(Token(TokenKind.Identifier, identifier), arguments);
+
+    public static IfExpr If(Expr condition, Expr thenBranch, Expr? elseBranch = null)
+        => new(condition, thenBranch, elseBranch);
+
+    public static BlockExpr Block(List<Expr> expressions)
+        => new(expressions, new TextPos(0, 0));
+
+    public static LiteralExpr Literal(object value)
+        => value switch
+        {
+            null => new(Token(TokenKind.Nil, "nil")),
+            true => new(Token(TokenKind.True, "true")),
+            false => new(Token(TokenKind.False, "false")),
+            int x => new(Token(TokenKind.NumberLiteral, x.ToString())),
+            double x => new(Token(TokenKind.NumberLiteral, x.ToString())),
+            string x => new(Token(TokenKind.StringLiteral, x)),
+            _ => new(Token(TokenKind.Unknown, value?.ToString() ?? "")),
+        };
 }
---
+++ tests/InterpreterTests.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Shel.Lexing;
+using Shel.Parsing;
+using Shel.Interpreting;
+using static AstBuilder;
+
+namespace Shel.Tests;
+
+internal class InterpreterTests
+{
+    [TestCase(2, TokenKind.Plus, 3, 5)]
+    [TestCase(2, TokenKind.Minus, 3, -1)]
+    [TestCase(2, TokenKind.Star, 3, 6)]
+    [TestCase(9, TokenKind.Slash, 2, 4.5)]
+    [TestCase("hello", TokenKind.Plus, " world", "hello world")]
+    [TestCase(5, TokenKind.Greater, 4.5, true)]
+    [TestCase(5, TokenKind.Greater, 5, false)]
+    [TestCase(5, TokenKind.GreaterEquals, 4.5, true)]
+    [TestCase(5, TokenKind.GreaterEquals, 5, true)]
+    [TestCase(5, TokenKind.Less, 4.5, false)]
+    [TestCase(5, TokenKind.Less, 5, false)]
+    [TestCase(5, TokenKind.LessEquals, 4.5, false)]
+    [TestCase(5, TokenKind.LessEquals, 5, true)]
+    [TestCase("hello", TokenKind.EqualsEquals, "hello", true)]
+    [TestCase("hello", TokenKind.NotEquals, "world", true)]
+    public void TestBinary(object left, TokenKind op, object right, object expectedResult)
+    {
+        var ast = Binary(
+            Literal(left),
+            op,
+            Literal(right)
+        );
+        var result = new Interpreter().Interpret(new List<Expr>() { ast });
+        Assert.AreEqual(RuntimeValue(expectedResult).GetType(), result.GetType());
+        Assert.True(SameResult(expectedResult, result));
+    }
+
+    [Test]
+    public void TestUnary()
+    {
+        var ast = Unary(
+            TokenKind.Minus,
+            Literal(2)
+        );
+        var result = new Interpreter().Interpret(new List<Expr>() { ast });
+        Assert.True(SameResult(-2, result));
+    }
+
+    [Test]
+    public void TestVariable()
+    {
+        var ast = new List<Expr>
+        {
+            Let("x", Literal(2)),
+            Var("x"),
+        };
+
+        var result = new Interpreter().Interpret(ast);
+        Assert.True(SameResult(2, result));
+    }
+
+    private bool SameResult(object expectedResult, IRuntimeValue gotResult)
+        => ((RuntimeBoolean)RuntimeValue(expectedResult).Operation(TokenKind.EqualsEquals, gotResult)).Value;
+
+    private IRuntimeValue RuntimeValue(object value)
+        => value switch
+        {
+            true => RuntimeBoolean.True,
+            false => RuntimeBoolean.False,
+            int x => new RuntimeNumber(x),
+            double x => new RuntimeNumber(x),
+            string x => new RuntimeString(x),
+            _ => new RuntimeNil(),
+        };
+}
