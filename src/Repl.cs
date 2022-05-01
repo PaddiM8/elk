bb688d94d69aa5d3c4cb6cafe6568b31c61f1ed6
PaddiM8 <bakk@tuta.io>
2022-05-01 20:07:11 +0200

    Add history to REPL

--- src/Repl.cs
+++ src/Repl.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using BetterReadLine;
 using Elk.Interpreting;
 
@@ -13,18 +14,34 @@ static class Repl
             args.Cancel = true;
         };
         
+        string configPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".config/elk"
+        );
+        if (!Directory.Exists(configPath))
+            Directory.CreateDirectory(configPath);
+
+        string historyFile = Path.Combine(configPath, "history.txt");
         var shell = new ShellSession();
-        var readLine = new ReadLine();
+        var readLine = new ReadLine()
+        {
+            HistoryEnabled = true,
+        };
+        
+        if (File.Exists(historyFile))
+            readLine.AddHistory(File.ReadAllLines(historyFile));
+        
         readLine.RegisterShortcut(
             new KeyPress(ConsoleModifiers.Control, ConsoleKey.D),
             _ => Environment.Exit(0)
         );
-        
+
         while (true)
         {
             shell.PrintPrompt();
             
             string input = readLine.Read();
+            File.AppendAllText(historyFile, $"{input}\n");
             if (input == "exit")
                 break;
 
