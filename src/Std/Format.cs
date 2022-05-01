ad1b3f4a4af151d5741ebda05f347620a35b3241
PaddiM8 <bakk@tuta.io>
2022-05-01 22:14:38 +0200

    Add terminal text formatting functions to std

---
+++ src/Std/Format.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using Elk.Attributes;
+using Elk.Interpreting;
+
+// ReSharper disable UnusedMember.Global
+
+namespace Elk.Std;
+
+public class Format
+{
+    [ShellFunction("color")]
+    public static RuntimeString Color(RuntimeString input, RuntimeString colorName)
+    {
+        return new(Ansi.Color(input.Value, colorName.Value));
+    }
+    
+   [ShellFunction("bold")]
+    public static RuntimeString Bold(RuntimeString input)
+        => new(Ansi.Bold(input.Value));
+    
+   [ShellFunction("italic")]
+    public static RuntimeString Italic(RuntimeString input)
+        => new(Ansi.Italic(input.Value));
+    
+   [ShellFunction("ansiReset")]
+    public static RuntimeString Reset()
+        => new(Ansi.Reset());
+}
+
+static class Ansi
+{
+    private static readonly Dictionary<string, int> _colors = new()
+    {
+        ["black"] = 30,
+        ["red"] = 31,
+        ["green"] = 32,
+        ["yellow"] = 33,
+        ["blue"] = 34,
+        ["magenta"] = 35,
+        ["cyan"] = 36,
+        ["white"] = 37,
+        ["default"] = 39,
+        ["brightBlack"] = 90,
+        ["brightRed"] = 91,
+        ["brightGreen"] = 92,
+        ["brightYellow"] = 93,
+        ["brightBlue"] = 94,
+        ["brightMagenta"] = 95,
+        ["brightCyan"] = 96,
+        ["brightWhite"] = 97,
+    };
+
+    public static string Bold(string value)
+        => Escape(value, 1, 22);
+
+    public static string Color(string value, string colorName)
+    {
+        return _colors.TryGetValue(colorName, out int colorCode)
+            ? Escape(value, colorCode, 39)
+            : value;
+    }
+    
+    public static string Italic(string value)
+        => Escape(value, 3, 23);
+
+    public static string Reset()
+        => "\x1b[0";
+
+    private static string Escape(string value, int code, int resetCode)
+        => $"\x1b[{code}m{value}\x1b[{resetCode}m";
+}
