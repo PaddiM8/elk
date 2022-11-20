646f2f863ea7752dc88ca12d998092139366d4f1
PaddiM8 <bakk@tuta.io>
2022-11-20 14:55:24 +0100

    cli: Highlight string literals inside text arguments

--- cli/HighlightHandler.cs
+++ cli/HighlightHandler.cs
@@ -14,30 +14,44 @@ class HighlightHandler : IHighlightHandler
 {
     private readonly ShellSession _shell;
     private readonly Regex _pattern;
+    private readonly Regex _textArgumentPattern;
 
     public HighlightHandler(ShellSession shell)
     {
         _shell = shell;
 
-        const string textArgument = "(?<textArgument>([A-Za-z0-9\\-~./](&(?!&)|\\-(?!\\>)|\\>|\\\\[{})|&\\->;\\n]|[^{})|&\\->;\\n])+)?)";
+        const string textArgument = "(?<textArgument>((&(?!&)|\\-(?!\\>)|\\>|\\\\[{})|&\\->;\\n]|[^{})|&\\->;\\n])+)?)";
         var rules = new[]
         {
             @"(?<keywords>\b(module|struct|fn|if|else|return|with|using|from|let|new|true|false|for|while|in|nil|break|continue|and|or)\b)",
             @"(?<types>\b(Boolean|Dictionary|Error|Float|Integer|List|Nil|Range|Regex|String|Tuple|Type|Iterable|Indexable)\b)",
             @"(?<numbers>(?<!\w)\d+(\.\d+)?)",
-            "(?<string>\"((?<=\\\\)\"|[^\"])*\"?)",
             "(?<singleQuoteString>\'((?<=\\\\)\'|[^\'])*\'?)",
+            "(?<string>\"((?<=\\\\)\"|[^\"])*\"?)",
             @"(?<comment>#.*(\n|\0))",
             @"(?<namedDeclaration>(?<=let |for |with |module |struct )(\w+|\((\w+[ ]*,?[ ]*)*))",
             @"(?<path>([.~]?\/|\.\.\/|(\\[^{})|\s]|[^{})|\s])+\/)(\\.|[^{})|\s])+ " + textArgument + ")",
             @$"(?<identifier>\b\w+( {textArgument})?)",
         };
-        _pattern = new Regex(string.Join("|", rules), RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+        _pattern = new Regex(
+            string.Join("|", rules),
+            RegexOptions.Compiled | RegexOptions.ExplicitCapture
+        );
+
+        var textArgumentRules = new[]
+        {
+            "(?<singleQuoteString>\'((?<=\\\\)\'|[^\'])*\'?)",
+            "(?<string>\"((?<=\\\\)\"|[^\"])*\"?)",
+        };
+        _textArgumentPattern = new Regex(
+            string.Join("|", textArgumentRules),
+            RegexOptions.Compiled | RegexOptions.ExplicitCapture
+        );
     }
 
     public string Highlight(string text)
     {
-        string x = _pattern.Replace(text, m =>
+        return _pattern.Replace(text, m =>
         {
             int? colorCode = null;
             if (m.Groups["keywords"].Value.Any())
@@ -75,11 +89,17 @@ class HighlightHandler : IHighlightHandler
                 if (_shell.StructExists(identifier))
                     return $"\x1b[96m{m.Groups["identifier"].Value}\x1b[0m";
 
-                if (_shell.VariableExists(identifier))
-                    return m.Groups["identifier"].Value;
+                if (_shell.VariableExists(identifier.Trim()))
+                {
+                    var highlightedArgument = argument.Any()
+                        ? Highlight(argument)
+                        : "";
+
+                    return identifier + highlightedArgument;
+                }
 
                 return argument.Any()
-                    ? $"\x1b[32m{identifier}\x1b[94m{argument}\x1b[0m"
+                    ? $"\x1b[32m{identifier}{HighlightTextArguments(argument)}\x1b[0m"
                     : $"\x1b[32m{identifier}\x1b[0m";
             }
 
@@ -87,7 +107,21 @@ class HighlightHandler : IHighlightHandler
                 ? m.Value
                 : $"\x1b[{colorCode}m{m.Value}\x1b[0m";
         });
+    }
 
-        return x;
+    private string HighlightTextArguments(string textArguments)
+    {
+        string result = _textArgumentPattern.Replace(textArguments, m =>
+        {
+            if (m.Groups["string"].Value.Any() ||
+                m.Groups["singleQuoteString"].Value.Any())
+            {
+                return $"\x1b[93m{m.Value}\x1b[94m";
+            }
+
+            return m.Value;
+        });
+
+        return $"\x1b[94m{result}\x1b[0m";
     }
 }
