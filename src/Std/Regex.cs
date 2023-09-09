e7665a4db06a7014359331fa354600df80bf2b92
PaddiM8 <hi@bakk.dev>
2023-09-09 12:02:41 +0200

    std: Change the parameter order in regex functions

--- src/Std/Regex.cs
+++ src/Std/Regex.cs
@@ -12,7 +12,7 @@ public class Regex
     /// <param name="value"></param>
     /// <returns>The values of the groups of the first match.</returns>
     [ElkFunction("groups")]
-    public static RuntimeList Groups(RuntimeRegex pattern, RuntimeString value)
+    public static RuntimeList Groups(RuntimeString value, RuntimeRegex pattern)
     {
         var result = pattern.Value
             .Matches(value.Value)
@@ -30,20 +30,20 @@ public class Regex
     /// <param name="value"></param>
     /// <returns>Whether a match of the pattern is found anywhere in the given value.</returns>
     [ElkFunction("isMatch")]
-    public static RuntimeBoolean IsMatch(RuntimeRegex pattern, RuntimeString value)
+    public static RuntimeBoolean IsMatch(RuntimeString value, RuntimeRegex pattern)
         => RuntimeBoolean.From(pattern.Value.IsMatch(value.Value));
 
     /// <param name="pattern"></param>
     /// <param name="value"></param>
     /// <returns>The first occurrence of the given pattern in the given value.</returns>
     [ElkFunction("find")]
-    public static RuntimeString Find(RuntimeRegex pattern, RuntimeString value)
+    public static RuntimeString Find(RuntimeString value, RuntimeRegex pattern)
         => new(pattern.Value.Match(value.Value).Value);
 
     /// <param name="pattern"></param>
     /// <param name="value"></param>
     /// <returns>All the occurrences of the given pattern in the given value.</returns>
     [ElkFunction("findAll")]
-    public static RuntimeList FindAll(RuntimeRegex pattern, RuntimeString value)
+    public static RuntimeList FindAll(RuntimeString value, RuntimeRegex pattern)
         => new(pattern.Value.Matches(value.Value).Select(x => new RuntimeString(x.Value)));
 }
