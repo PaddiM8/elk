ee9bf624a785570b8cf1a53a347197d0df86447b
PaddiM8 <hi@bakk.dev>
2023-12-02 22:26:52 +0100

    Don't print some exit signal messages

--- src/Interpreting/SignalHelper.cs
+++ src/Interpreting/SignalHelper.cs
@@ -4,9 +4,9 @@ public class SignalHelper
 {
     public static string[] SignalNames { get; } =
     [
-        "SIGHUP (Terminal hung up)",
-        "SIGINT (Quit request from job control (^C))",
-        "SIGQUIT (Quit request from job control with core dump (^\\))",
+        "", // SIGHUP
+        "", // SIGINT
+        "", // SIGQUIT
         "SIGILL (Illegal instruction)",
         "SIGTRAP (Trace or breakpoint trap)",
         "", // SIGABRT
@@ -14,25 +14,25 @@ public class SignalHelper
         "SIGBUS (Misaligned address error)",
         "SIGFPE (Floating point exception)",
         "SIGKILL (Forced quit)",
-        "SIGUSR1 (User defined signal 1)",
+        "", // SIGUSR1
         "SIGSEGV (Address boundary error)",
-        "SIGUSR2 (User defined signal 2)",
+        "", // SIGUSR2
         "SIGPIPE (Broken pipe)",
         "SIGALRM (Timer expired)",
-        "SIGTERM (Polite quit request)",
-        "STKFLT",
-        "SIGCHLD (Child process status changed)",
-        "SIGCONT (Continue previously stopped process)",
+        "", // SIGTERM
+        "", // STKFLT
+        "", // SIGCHLD
+        "", // SIGCONT
         "SIGSTOP (Forced stop)",
-        "SIGTSTP (Stop request from job control (^Z))",
-        "SIGTTIN (Stop from terminal input)",
-        "SIGTTOU (Stop from terminal output)",
+        "", // SIGTSTP
+        "", // SIGTTIN
+        "", //SIGTTOU
         "SIGURG (Urgent socket condition)",
         "SIGXCPU (CPU time limit exceeded)",
         "SIGXFSZ (File size limit exceeded)",
         "SIGVTALRM (Virtual timefr expired)",
         "SIGPROF (Profiling timer expired)",
-        "SIGWINCH (Window size change)",
-        "SIGIO (I/O on asynchronous file descriptor is possible)",
+        "", // SIGWINCH
+        "", // SIGIO
     ];
 }
