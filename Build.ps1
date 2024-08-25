4c1f8b06e75488b408d8b17ac087c07e6188954a
PaddiM8 <hi@bakk.dev>
2024-08-25 13:08:11 +0200

    Build.ps1: Support cross-architecture compilation

--- Build.ps1
+++ Build.ps1
@@ -1,9 +1,26 @@
-dotnet publish cli/Elk.Cli.csproj -c Release
+param($arch)
 
-if (Test-Path "build") {
-    rd -r build
+$systemArch = [System.Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")
+if ([string]::IsNullOrWhiteSpace($arch)) {
+  if ($systemArch -eq "ARM64") {
+    $arch = "arm64"
+  } else {
+    $arch = "x64"
+  }
 }
 
-mkdir build
-cp cli/bin/Release/*/*/publish/Elk.Cli.exe build/elk.exe
-cp -r cli/bin/Release/*/*/publish/Resources/* build
+$target = "win-$arch"
+dotnet publish cli/Elk.Cli.csproj -c Release -r $target
+
+
+if (-not (Test-Path "build")) {
+  mkdir build
+}
+
+if (Test-Path "build/$target") {
+  rd -r build/$target
+}
+
+mkdir build/$target
+cp cli/bin/Release/*/$target/publish/Elk.Cli.exe build/$target/elk.exe
+cp -r cli/bin/Release/*/$target/publish/Resources/* build/$target
