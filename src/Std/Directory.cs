be0e58a47fed9bd99155e05912abb2b8a0c810f7
PaddiM8 <hi@bakk.dev>
2025-03-09 19:37:36 +0100

    std: Add 'dir::entries', 'dir::files' and 'dir::dirs'

--- src/Std/Directory.cs
+++ src/Std/Directory.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Elk.Std.Attributes;
 using Elk.Std.DataTypes;
 using Elk.Vm;
@@ -8,6 +9,45 @@ namespace Elk.Std;
 static class Directory
 {
     [ElkFunction("exists")]
-    public static RuntimeBoolean Exists(RuntimeString path, ShellEnvironment env)
+    public static RuntimeBoolean Exists(RuntimeString path)
         => RuntimeBoolean.From(System.IO.Directory.Exists(ShellEnvironment.GetAbsolutePath(path.Value)));
+
+    [ElkFunction("entries")]
+    public static RuntimeList Entries(RuntimeString? path = null)
+    {
+        var currentDir = path?.Value ?? ShellEnvironment.WorkingDirectory;
+
+        var files = System.IO.Directory
+            .GetFileSystemEntries(ShellEnvironment.GetAbsolutePath(currentDir))
+            .Select<string, RuntimeObject>(x => new RuntimeString(x))
+            .ToList();
+
+        return new RuntimeList(files);
+    }
+
+    [ElkFunction("dirs")]
+    public static RuntimeList Dirs(RuntimeString? path = null)
+    {
+        var currentDir = path?.Value ?? ShellEnvironment.WorkingDirectory;
+
+        var files = System.IO.Directory
+            .GetDirectories(ShellEnvironment.GetAbsolutePath(currentDir))
+            .Select<string, RuntimeObject>(x => new RuntimeString(x))
+            .ToList();
+
+        return new RuntimeList(files);
+    }
+
+    [ElkFunction("files")]
+    public static RuntimeList Files(RuntimeString? path = null)
+    {
+        var currentDir = path?.Value ?? ShellEnvironment.WorkingDirectory;
+
+        var files = System.IO.Directory
+            .GetFiles(ShellEnvironment.GetAbsolutePath(currentDir))
+            .Select<string, RuntimeObject>(x => new RuntimeString(x))
+            .ToList();
+
+        return new RuntimeList(files);
+    }
 }
